504da5dee1d2b6ef3a8cb24bb88b68d5ee8d5a1f
vinniciusrodrigo <vinniciusrodrigo45@gmail.com>
2020-06-06 13:21:07 -0300

    first commit

---
+++ Assets/ClickTime.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ClickTime : MonoBehaviour
+{
+    Corountine startedCoroutine;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+    public void PointerEnter()
+    {
+        //eventTrigger.OnPointerClick(new PointerEventData(EventSystem.current));
+        startedCorountine= StartCorountine(CountByClick());
+
+    }
+
+    public void PointerExit(){
+        StopCorountine(startedCorountine);
+    }
+
+    IEnumerator CountByClick(){
+        yield return new WaitForSeconds(3f);
+        ExecuteEvents.Execute<IPointerClickHandler>(this.gameObject, new PointerEventData(EventSystem.current), ExecuteEvents.pointerClickHandler); 
+    }
+}
---
+++ Assets/Waypoint.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoint : MonoBehaviour
+{
+    private static GameObject Player;
+    private static GameObject previousWaypoints;
+    void Awake()
+    {
+        //Executado antes do start
+        if(!Player){
+            Player = GameObject.FindGameObjectWithTag("Player");
+
+        }
+    }
+
+    public void Move()
+    {
+        Player.transform.position = this.transform.position;
+
+        if(previousWaypoints){
+            previousWaypoints.SetActive(true);
+        }
+
+        previousWaypoints = this.gameObject;
+        this.gameObject.SetActive(false);
+      }
+}
